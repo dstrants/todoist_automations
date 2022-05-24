ae668e9230048f907a6f0038609a55a7fa3d6ba4
Dimitris <dimitris.strantsalis@workable.com>
2022-05-24 10:18:28 +0300

    🔧  Removes hardcoded local db credentials

--- utils/mongo.py
+++ utils/mongo.py
@@ -1,8 +1,10 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.collection import Collection
 
+from config.constants import MONGO_CLIENT
+
 def mongo_collection(collection_name: str = "items") -> Collection:
-    client = MongoClient("mongodb://root:example@localhost:27017/")
+    client = MongoClient(MONGO_CLIENT)
     # TODO: Make the database name dynamic
     db = client['todoist']
     return db[collection_name]
