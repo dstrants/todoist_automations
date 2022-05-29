bedc4659e66260e0b9e9365ad6fd86094a2e3771
Dimitris <dimitris.strantsalis@workable.com>
2022-05-29 11:19:46 +0300

    ⚰️  Remove deprecates mongo client

--- utils/mongo.py
+++
@@ -1,7 +0,0 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.collection import Collection
-
-from config.base import config
-
-def mongo_collection(collection_name: str = "items") -> Collection:
-    return config.mongo.todoist_collection(collection_name)
