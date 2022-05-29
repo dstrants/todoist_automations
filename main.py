42bf44fcfe7d08622a733ec176f6f489b66b60ab
Dimitris <dimitris.strantsalis@workable.com>
2022-05-29 19:34:35 +0300

    🔊  Adds some logging to webendpoints

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ app.on_event("startup")(startup_ensure_telegram_webhook)
 @app.post("/todoist/webhooks")
 async def todoist_webhooks(request: Request, response: Response, webhook: TodoistWebhook, background_tasks: BackgroundTasks):
     if not await todoist_validate_webhook_hmac(request=request):
+        config.logger.info("Invalid webhook HMAC")
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"message": "Request does not have the right format"}
+    config.logger.info("Received webhook from todoist with event type: %s", webhook.event_name)
     match webhook.event_name:
         case "item:added" | "item:updated":
             background_tasks.add_task(create_or_update_task, webhook=webhook)
@@ -49,14 +51,13 @@ async def todoist_login_redirect():
 @app.get("/todoist/callback")
 async def todoist_redirect_callback(response: Response, background_tasks: BackgroundTasks, code: str = "", state: str = ""):
     if state != config.todoist.state_string:
+        config.logger.info("Invalid state string from todoist callback")
         response.status_code = status.HTTP_403_FORBIDDEN
         return {"message": "Access denied"}
 
     user_info = await todoist_oauth_flow_step_2(code, full_sync=True)
-    print(user_info)
     background_tasks.add_task(create_user, user=user_info)
     telegram_link = start_telegram_authentication_process(user_info["id"])
-
 
     return {"message": "Login successful", "telegram_link": telegram_link}
 
@@ -68,6 +69,7 @@ async def telegram_webhook(request: Request, background_tasks: BackgroundTasks):
     text = data["message"]["text"]
 
     if text.startswith("/start"):
+        config.logger.info("Received /start command from telegram")
         background_tasks.add_task(telegram.complete_telegram_verification, webhook_body=data)
 
     return {"message": "Webhook received"}
