0aae344f9ca8e4b3ef60f568bc98229167cbd8f0
Dimitris <dimitris.strantsalis@workable.com>
2022-07-28 10:40:14 +0300

    🐛  Handles the failed authentication with todoist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sentry_sdk
 from fastapi import BackgroundTasks, FastAPI, Request, Response, status
 from fastapi.responses import RedirectResponse
+from requests.exceptions import HTTPError
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from config.base import config
@@ -24,7 +25,7 @@ app.on_event("startup")(start_up.startup_ensure_telegram_webhook)
 
 try:
     app.add_middleware(SentryAsgiMiddleware)
-except Exception as e:
+except Exception as e: # skipcq: PYL-W0703 - Every import error should be handled
     config.logger.warning(f"Failed to add SentryAsgiMiddleware: {e}")
 
 
@@ -66,7 +67,12 @@ async def todoist_redirect_callback(response: Response, background_tasks: Backgr
         response.status_code = status.HTTP_403_FORBIDDEN
         return {"message": "Access denied"}
 
-    user_info = await todoist_auth.todoist_oauth_flow_step_2(code, full_sync=True)
+    try:
+        user_info = await todoist_auth.todoist_oauth_flow_step_2(code, full_sync=True)
+    except HTTPError:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {"message": "Invalid code"}
+
     background_tasks.add_task(todoist_users.create_user, user=user_info)
     telegram_link = telegram_auth.start_telegram_authentication_process(user_info["id"])
 
