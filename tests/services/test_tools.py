4502c7adede93883a9e4c9765d9ef83da1e5a7e8
Dimitris <dimitris.strantsalis@workable.com>
2022-08-21 18:55:49 +0300

    ✅  Adds testing for tools

---
+++ tests/services/test_tools.py
@@ -0,0 +1,24 @@
+from services.gh.repos import get_repo
+from services.toolslib.tools import form_repo_dict
+
+
+EXISTING_REPO = "tiangolo/fastapi"
+
+
+def test_form_repo_dict() -> None:
+    repo = get_repo(EXISTING_REPO)
+    repo_dict = form_repo_dict(repo)
+
+    assert isinstance(repo_dict, dict)
+    assert repo_dict["Name"] == EXISTING_REPO == repo.name
+
+    assert {
+        "Name": repo.name,
+        "Github": repo.html_url,
+        "Tags": repo.get_topics(),
+        "Notes": repo.description,
+        "Webpage": repo.homepage,
+        "Language": repo.language
+
+    } == repo_dict
+
