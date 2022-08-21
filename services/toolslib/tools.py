f7364fb50d63b4e0e819c8c40efb960997644efb
Dimitris <dimitris.strantsalis@workable.com>
2022-08-21 15:40:06 +0300

    🚧  Some additional func on business logic

--- services/toolslib/tools.py
+++ services/toolslib/tools.py
@@ -1,9 +1,27 @@
+from github.Repository import Repository
+
+from config.base import config
 from services.gh.repos import get_repo
 from services.airtable.records import create_record
 
+# TODO: Cache repos to the mongo db
+
+def form_repo_dict(repo: Repository) -> dict:
+    return {
+        "Name": repo.name,
+        "Github": repo.html_url,
+        "Tags": repo.get_topics(),
+        "Notes": repo.description,
+        "Webpage": repo.homepage,
+        "Language": repo.language
+
+    }
 
 def create_record_from_repo(repo_name: str) -> None:
-    repo = get_repo(repo_name, dictionary=True)
-    create_record(repo)
+    repo = get_repo(repo_name)
+    repo_dict = form_repo_dict(repo)
+
+    create_record(repo_dict, typecast=True)
+    config.logger.info("Repo %s has been created", repo_name)
 
     return None
