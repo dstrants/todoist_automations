101dc1904bfe43bb536032f25359563ff148ecb7
Dimitris <dimitris.strantsalis@workable.com>
2022-08-21 15:40:32 +0300

    🚧 Adds typecast attr on client

--- services/airtable/records.py
+++ services/airtable/records.py
@@ -1,5 +1,5 @@
 from config.base import config
 
 
-def create_record(record: dict) -> dict:
-    return config.airtable.base.create(record)
+def create_record(record: dict, typecast: bool = False) -> dict:
+    return config.airtable.base.create(record, typecast=typecast)
