30f26b730148d5cd714422ab83fd2f5a9df3ee0c
Dimitris <dimitris.strantsalis@workable.com>
2022-08-22 17:24:05 +0300

    📝  Documents the telegram logic

--- services/telegram/messages.py
+++ services/telegram/messages.py
@@ -4,6 +4,7 @@ from config.base import config
 
 
 def send_telegram_message(message: str, chat_id: int) -> Message:
+    """Sends a message to a user via telegram"""
     bot = Bot(token=config.telegram.bot_token)
     config.logger.info("Sending message to chat %s", chat_id)
     return bot.send_message(chat_id=chat_id, text=message)
