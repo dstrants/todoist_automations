2040c4b88df4607a2e8ecaa55674b40ab4c6c914
Dimitris <dimitris.strantsalis@workable.com>
2022-08-22 09:38:11 +0300

    🚨 Linting suggestions

--- middleware/security.py
+++ middleware/security.py
@@ -6,9 +6,10 @@ from config.base import config
 api_key_header = APIKeyHeader(name="X-DOISTER-API-KEY", auto_error=True)
 
 
-def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
-    if api_key_header == config.api_key:
-        return api_key_header
+def get_api_key(api_key: str = Security(api_key_header)) -> str:
+    """Validates the incoming api key and returns the api key if valid"""
+    if api_key == config.api_key:
+        return api_key
     raise HTTPException(
         status_code=status.HTTP_403_FORBIDDEN, detail="Could not authenticate user"
     )
