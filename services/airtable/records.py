30b2285de5c7d78ebca2e425c55185852ef6f2b3
Dimitris <dimitris.strantsalis@workable.com>
2022-08-22 17:19:40 +0300

    📝  Documents the airtable logic

--- services/airtable/records.py
+++ services/airtable/records.py
@@ -2,4 +2,7 @@ from config.base import config
 
 
 def create_record(record: dict, typecast: bool = False) -> dict:
+    """
+        Creates a record on the tools database on airtable
+    """
     return config.airtable.base.create(record, typecast=typecast)
