8d8d3f83889f996742fd5bd34cf0924c5a8aa5d3
Dimitris <dimitris.strantsalis@workable.com>
2022-08-22 17:18:39 +0300

    📝  Documents the tools route

--- routes/tools.py
+++ routes/tools.py
@@ -13,10 +13,16 @@ router = APIRouter(
 
 
 class NewRepo(BaseModel):
+    """Models the body of the tools webhook endpoint."""
     full_name: str
 
 
 @router.post("/new")
 async def tools_webhook(repo: NewRepo, background_tasks: BackgroundTasks, _api_key: APIKeyBase = Depends(get_api_key)):
+    """
+        Webhook endpoint for the tools repository.
+
+        Receives the `full_name` of the new repository and initiates the creation of the record in the database.
+    """
     background_tasks.add_task(create_record_from_repo, repo.full_name)
     return {"message": "Repo has been created"}
